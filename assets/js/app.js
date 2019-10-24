99ff4609d5000e175021d119e3fcd344aa09d54e
skirmantek <skirmante.klimciukaite@gmail.com>
2019-10-24 21:13:25 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,30 @@
+
+     $(function() {
+        $('[data-toggle]').on('click', function(e){
+            e.preventDefault();
+            let element = $(this).data('toggle');
+        console.log(
+            $('[data-' + element + ']').toggleClass(element + '-hide'));
+        });  
+        });     
+
+
+
+        function openTab(evt, tabName) {
+          var i, tabcontent, tablinks;
+          tabcontent = document.getElementsByClassName("tabcontent");
+          for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
+          tablinks = document.getElementsByClassName("tablinks");
+          for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
+          document.getElementById(tabName).style.display = "inline-block";
+          evt.currentTarget.className += " active";
+        }
+        document.getElementById("defaultOpen").click();
+
+
+
+
